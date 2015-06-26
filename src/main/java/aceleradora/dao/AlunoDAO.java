7732044ce93fd76a0f22ff1f88052d9a37cc4566
lucassveloso <lucas.s.veloso@hotmail.com>
2015-06-25 22:19:46 -0300

    Adicionando metodo para retornar uma lista com os alunos escolhidos para manter nas histórias

--- src/main/java/aceleradora/dao/AlunoDAO.java
+++ src/main/java/aceleradora/dao/AlunoDAO.java
@@ -14,21 +14,34 @@ public class AlunoDAO {
 
     public AlunoDAO() {
         alunos = new ArrayList<>();
-        alunos.add(new Aluno(0,"Amanda"));
-        alunos.add(new Aluno(1,"Johan"));
-        alunos.add(new Aluno(2,"Lucas"));
-        alunos.add(new Aluno(3,"Thiago"));
-        alunos.add(new Aluno(4,"Yasser"));
-        alunos.add(new Aluno(5,"Eduardo"));
-        alunos.add(new Aluno(6,"Juliana"));
-        alunos.add(new Aluno(7,"Marcus"));
-        alunos.add(new Aluno(8,"Rafael R."));
-        alunos.add(new Aluno(9,"Guilherme"));
-        alunos.add(new Aluno(10,"Raphael B."));
+        alunos.add(new Aluno(0, "Amanda"));
+        alunos.add(new Aluno(1, "Johan"));
+        alunos.add(new Aluno(2, "Lucas"));
+        alunos.add(new Aluno(3, "Thiago"));
+        alunos.add(new Aluno(4, "Yasser"));
+        alunos.add(new Aluno(5, "Eduardo"));
+        alunos.add(new Aluno(6, "Juliana"));
+        alunos.add(new Aluno(7, "Marcus"));
+        alunos.add(new Aluno(8, "Rafael R."));
+        alunos.add(new Aluno(9, "Guilherme"));
+        alunos.add(new Aluno(10, "Raphael B."));
     }
 
     public List<Aluno> getAlunos() {
         return alunos;
+    }
+
+    public List<Aluno> getAlunosManter(List<String> listamanter) {
+        List<Aluno> alunosmanter = new ArrayList<>();
+        for (int j = 0; j <alunos.size() ; j++) {
+            for (int i = 0; i < listamanter.size(); i++) {
+                if (alunos.get(j).getCodigo() == Integer.parseInt(listamanter.get(i))) {
+                    alunosmanter.add(alunos.get(j));
+                }
+            }
+        }
+
+        return alunosmanter;
     }
 
     public void setAlunos(List<Aluno> alunos) {
